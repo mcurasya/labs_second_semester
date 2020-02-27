33d6568292e66a373145719c0f37cb59f22d0177
Artem <smurfik997@gmail.com>
2020-02-27 11:31:18 +0200

    laba16 ready

---
+++ laba16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#undef and
+#undef or
+#undef not
+#define and(x, y) (x) & (y)
+#define or(x, y) (x) | (y)
+#define not(x) ~(x)
+#define show(x) printf("w=%d", x)
+#define ask printf("input a, x, y, z>>")
+
+
+int main() {
+	int a, x, y, z, w = 0; 
+	ask;
+	scanf("%d%d%d%d", &a, &x, &y, &z);
+	if (a) {
+		w = or(and (x + y, y * z), not(z));
+	}
+	else {
+		w = not(and (or (0, x), x - y));
+	}
+	show(w);
+	return 0;
+}
